c3b33c1ccbaca83815bbd9775e1a695c31d92a8c
James London <james@pop-os.localdomain>
2024-03-02 15:34:51 -0600

    a

--- src/buffer/stage.rs
+++ src/buffer/stage.rs
@@ -1,4 +1,6 @@
-use crate::{display::event_loop::Input, file::toml::Toml};
+use winit::window::Window;
+
+use crate::{display::{event_loop::Input, text_render::Canvas}, file::toml::Toml, state::application::State};
 
 pub trait Stage where Self: Default {
     fn poll(&mut self, input: &Input) -> anyhow::Result<()>;
@@ -9,4 +11,18 @@ pub trait Configurable where Self: Default + Stage {
     fn configure(&mut self, config: Toml) -> anyhow::Result<()>;
     fn default_configuration() -> Toml;
     const CONFIG_FILE_NAME: &'static str;
+}
+
+pub trait Render where Self: Stage {
+    fn render(&self, canvas: Canvas<&Window, &Window>, stage: &State);
+}
+
+pub trait TextStage where Self: Stage {
+    fn get_display_text(&self) -> String;
+}
+
+impl<T: TextStage> Render for T {
+    fn render(&self, mut canvas: Canvas<&Window, &Window>, state: &State) {
+
+    }
 }
