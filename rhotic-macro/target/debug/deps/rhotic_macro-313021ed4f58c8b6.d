1d620649e6784915efd66cdddfc2abe263ee2d32
James London <james@jameslondon.me>
2024-03-05 10:02:21 -0600

    a

--- rhotic-macro/target/debug/deps/rhotic_macro-313021ed4f58c8b6.d
+++
@@ -1,6 +0,0 @@
-/home/james/Library/Rust/rhotic/rhotic-macro/target/debug/deps/librhotic_macro-313021ed4f58c8b6.rmeta: src/lib.rs src/text_stage_impl.rs
-
-/home/james/Library/Rust/rhotic/rhotic-macro/target/debug/deps/rhotic_macro-313021ed4f58c8b6.d: src/lib.rs src/text_stage_impl.rs
-
-src/lib.rs:
-src/text_stage_impl.rs:
